5882a460b351085c39c6d71c0cb43adcf018f671
Alessandro <alessandrorl@ufps.edu.co>
2024-11-08 11:44:05 -0500

    Entidades y repositorios terminados

---
+++ src/main/java/co/edu/ufps/repositories/ProjectAssignmentRepository.java
@@ -0,0 +1,9 @@
+package co.edu.ufps.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import co.edu.ufps.entities.ProjectAssignment;
+public interface ProjectAssignmentRepository extends JpaRepository<ProjectAssignment, ProjectAssignmentKey> {
+
+}
